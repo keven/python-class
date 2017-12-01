40e8fe293d83192e808ceeb33735555c8559ee9b
Keven Lin <kevenlin@kevens-mbp.lan>
2017-11-30 22:19:52 -0800

    first part of movie website project

---
+++ week9/movies/entertainment_center.py
@@ -0,0 +1,13 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story...",
+                        "movie image link here",
+                        "youtube link here")
+print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                    "movie image link here",
+                    "youtube link here")
+print(avatar.storyline)
